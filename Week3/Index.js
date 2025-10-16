796242422c503ffe2e97405bcbc972222d2d797b
animeshpaudel94-commits <animeshpaudel94@gmail.com>
2025-10-16 15:45:34 +0000

    push

--- Week3/Index.js
+++ Week3/Index.js
@@ -1,1 +1,10 @@
+function EmployeeInfo(name,Salary)
+{
+    console.log("Wellcome" + name+ "Your month Salary is "+ Salary)
+}
+console.log ("THis is my first program")
 
+var EmpName="John"
+var EmpSalary= 5000
+// calling of the function EmployeeInfo
+EmployeeInfo(EmpName,EmpSalary)
